39effeb4bdb8266933294490eb244a4c4921f44c
cde123456781 <bjc199@uclive.ac.nz>
2026-06-14 15:17:19 +1200

    Change allowed hosts to attempt to fix vercel deployment

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,15 @@ import { username } from "better-auth/plugins";
 import {db} from "~/server/db/index"
 
 export const auth = betterAuth({
+    baseURL: {
+		allowedHosts: [
+			"localhost:3000",
+			"localhost:5173",
+			"myapp.com",
+			"*.vercel.app",
+		],
+		protocol: process.env.NODE_ENV === "development" ? "http" : "https",
+	},
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
     }),
