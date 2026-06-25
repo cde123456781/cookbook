9669061e41acb580da26cec22f993a9db106db1a
cde123456781 <bjc199@uclive.ac.nz>
2026-06-25 20:26:30 +1200

    Adds Unit to types

--- src/app/types/recipe.ts
+++ src/app/types/recipe.ts
@@ -1,11 +1,7 @@
-import {
-  type steps,
-  type categories,
-  type ingredients,
-  recipes_ingredients,
-} from "~/server/db/schema";
+import { type steps, type categories, type ingredients, recipes_ingredients, units } from "~/server/db/schema";
 
 export type Category = typeof categories.$inferSelect;
 export type Ingredient = typeof ingredients.$inferSelect;
 export type Step = typeof steps.$inferSelect;
 export type RecipeIngredient = typeof recipes_ingredients.$inferSelect;
+export type Unit = typeof units.$inferSelect;
